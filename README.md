9f0debea152ad7de80149ad9973e08b7ec2da680
GusBrocchini <augustbrocchini@gmail.com>
2022-04-04 14:51:39 -0400

    added data_prep.py to organize data for pytorch

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 interpretable music classification for CPSC 452
 
 ## To get data:
-Download the GTZAN dataset from [kaggle](https://www.kaggle.com/datasets/andradaolteanu/gtzan-dataset-music-genre-classification) and move it into this folder under the name `GTZAN`.
+Download the GTZAN dataset from [kaggle](https://www.kaggle.com/datasets/andradaolteanu/gtzan-dataset-music-genre-classification) and move it into this folder under the name `GTZAN`. Then, run `data_prep.py` to organize the spectrograms into the format pytorch wants them in.
---
+++ data_prep.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import random
+
+random.seed(5672349)
+
+spec_dir = 'GTZAN/images_original'
+
+def make_or_clean_dir(d):
+    try:
+        os.mkdir(d)
+    except FileExistsError:
+        shutil.rmtree(d)
+        os.mkdir(d)
+
+data_folders = ['data/train', 'data/test']
+make_or_clean_dir('data')
+for d in data_folders:
+    make_or_clean_dir(d)
+
+for genre in os.listdir(spec_dir):
+    spec_names = list(os.listdir(os.path.join(spec_dir, genre)))
+    random.shuffle(spec_names)
+    test_paths = spec_names[:20]
+    train_paths = spec_names[20:]
+
+    for d in data_folders:
+        make_or_clean_dir(os.path.join(d, genre))
+
+    for spec_name in test_paths:
+        shutil.copyfile(os.path.join(spec_dir, genre, spec_name),
+                        os.path.join(data_folders[1], genre, spec_name))
+
+    for spec_name in train_paths:
+        shutil.copyfile(os.path.join(spec_dir, genre, spec_name),
+                        os.path.join(data_folders[0], genre, spec_name))
+
+# done.
