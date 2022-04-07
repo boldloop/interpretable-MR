2823a92bc78cc5a2ee17ef62b2236fcc9fe1e075
Samuel Halladay <samuel.halladay@yale.edu>
2022-04-07 09:26:52 -0400

    Implemented cropper

--- cnn_v2.py
+++ cnn_v2.py
@@ -5,6 +5,8 @@ from rich.progress import track
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
 
 # configure argparse for Farnam
 parser = argparse.ArgumentParser()
@@ -22,21 +24,30 @@ batch_size = args.batch_size
 learning_rate = args.learning_rate
 
 # import data and create trainloader
+class Cropper:
+    def __call__(self, img):
+        return transforms.functional.crop(img, 35, 54, 218, 336)
+
+# import data and create trainloader
 train_dataset = datasets.ImageFolder(
     "data/train",
     transform=transforms.Compose(
-        [transforms.Grayscale(), transforms.Resize((36, 54)), transforms.ToTensor()]
+        [transforms.Grayscale(), Cropper(),  transforms.ToTensor()]
     ),
 )
 test_dataset = datasets.ImageFolder(
     "data/test",
     transform=transforms.Compose(
-        [transforms.Grayscale(), transforms.Resize((36, 54)), transforms.ToTensor()]
+        [transforms.Grayscale(), Cropper(),  transforms.ToTensor()]
     ),
 )
 
 train_data = torch.stack([train_dataset[i][0] for i in range(len(train_dataset))])
 train_labels = torch.Tensor([train_dataset[i][1] for i in range(len(train_dataset))])
+
+plt.imshow(np.squeeze(train_data[4]))
+plt.show()
+exit()
 
 test_data = torch.stack([test_dataset[i][0] for i in range(len(test_dataset))])
 test_labels = torch.Tensor([test_dataset[i][1] for i in range(len(test_dataset))])
@@ -52,21 +63,24 @@ testloader = torch.utils.data.DataLoader(
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 30, 5)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(1, 30, 3, padding=0)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool2 = nn.MaxPool2d(5, 5)
         self.bn = nn.BatchNorm2d(30)
-        self.conv2 = nn.Conv2d(30, 30, 10)
-        self.fc1 = nn.Linear(720, 300)
-        self.fc2 = nn.Linear(300, 100)
+        self.conv2 = nn.Conv2d(30, 30, 3, padding=0)
+        self.conv3 = nn.Conv2d(30, 30, 3, padding=0)
+        self.fc1 = nn.Linear(240, 100)
+        self.fc2 = nn.Linear(100, 100)
         self.fc3 = nn.Linear(100, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.bn(self.conv1(x))))
-        x = self.pool(F.relu(self.bn(self.conv2(x))))
-        # x = self.pool(F.relu(self.bn(self.conv2(x))))
+        # Implement skip connections by flattening and concatenating intermediate results
+        x = self.pool1(F.relu(self.bn(self.conv1(x))))
+        x = self.pool2(F.relu(self.bn(self.conv2(x))))
+        x = self.pool1(F.relu(self.bn(self.conv3(x))))
         x = x.view(x.shape[0], -1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
@@ -89,6 +103,7 @@ for epoch in track(range(num_epochs)):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
     if (epoch + 1) % 5 == 0:
         with torch.no_grad():
             n_correct = 0
