b1d61cebfe0c43a2192ee46621197fc6bb41a449
Samuel Halladay <samuel.halladay@yale.edu>
2022-04-08 10:50:22 -0400

    Reformatted with Black

--- data_prep.py
+++ data_prep.py
@@ -4,7 +4,8 @@ import random
 
 random.seed(5672349)
 
-spec_dir = 'GTZAN/images_original'
+spec_dir = "GTZAN/images_original"
+
 
 def make_or_clean_dir(d):
     try:
@@ -13,8 +14,9 @@ def make_or_clean_dir(d):
         shutil.rmtree(d)
         os.mkdir(d)
 
-data_folders = ['data/train', 'data/test']
-make_or_clean_dir('data')
+
+data_folders = ["data/train", "data/test"]
+make_or_clean_dir("data")
 for d in data_folders:
     make_or_clean_dir(d)
 
@@ -28,11 +30,15 @@ for genre in os.listdir(spec_dir):
         make_or_clean_dir(os.path.join(d, genre))
 
     for spec_name in test_paths:
-        shutil.copyfile(os.path.join(spec_dir, genre, spec_name),
-                        os.path.join(data_folders[1], genre, spec_name))
+        shutil.copyfile(
+            os.path.join(spec_dir, genre, spec_name),
+            os.path.join(data_folders[1], genre, spec_name),
+        )
 
     for spec_name in train_paths:
-        shutil.copyfile(os.path.join(spec_dir, genre, spec_name),
-                        os.path.join(data_folders[0], genre, spec_name))
+        shutil.copyfile(
+            os.path.join(spec_dir, genre, spec_name),
+            os.path.join(data_folders[0], genre, spec_name),
+        )
 
 # done.
